327b5e0f34275fe4a5d19963b3709f3433774eac
Shameek Sarkar <sham3k@gmail.com>
2021-09-06 18:39:58 +0530

    self review

--- exposed-jdbc/src/test/kotlin/org/jetbrains/exposed/sql/transactions/experimental/KPostreSQLContainer.kt
+++
@@ -1,5 +0,0 @@
-package org.jetbrains.exposed.sql.transactions.experimental
-
-import org.testcontainers.containers.PostgreSQLContainer
-
-class KPostgreSQLContainer(imageName: String) : PostgreSQLContainer<KPostgreSQLContainer>(imageName)
